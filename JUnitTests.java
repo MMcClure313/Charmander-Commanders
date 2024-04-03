38e22493e725b3640a22c1af93fc3e87f77ff1a6
MMcClure313 <122900871+MMcClure313@users.noreply.github.com>
2024-04-03 18:28:47 -0500

    Tests guessing lopgic and generation logic

--- JUnitTests.java
+++ JUnitTests.java
@@ -5,27 +5,29 @@ import org.junit.jupiter.api.Test;
 
 class JUnitTests {
 
-	HangmanGame gameTest;
-	WordGenerator generate;
+	static HangmanGame gameTest;
+	static WordGenerator generate;
+	static Observer obv;
+	
 	@BeforeAll
-	void setUpBeforeClass() throws Exception {
-		Observer obv = null;
-		gameTest = new HangmanGame("default_words.csv", obv, "hard");
-		generate = new WordGenerator("default_words.csv");
+	static void setUpBeforeClass() throws Exception {
+		gameTest = new HangmanGame("test_words.csv", obv, "hard");
+		generate = new WordGenerator("test_words.csv");
 	}
 
 	@Test
 	void guessTest() {
 		
-		assertEquals(gameTest.guessLetter('b'), true);
-		assertEquals(gameTest.guessLetter('b'), false);
-	
+		assertEquals(gameTest.guessLetter('t'), true);
+		assertEquals(gameTest.guessLetter('t'), false);
+		assertEquals(gameTest.guessLetter('x'), false);
 	}
 	
 	@Test
 	void generateTest() {
 		String s = generate.getRandomWord();
-		assert(!s.isEmpty());
+		assertEquals(s.contains("testing"), true);
 	}
+
 
 }
